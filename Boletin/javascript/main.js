9ccb97883141e2a1ed203c50e414614d87867d0b
Al3x4nderCM <alexandercherres.04@gmail.com>
2022-12-03 18:12:03 -0500

    Redireccion a /Boletin

--- Boletin/javascript/main.js
+++ Boletin/javascript/main.js
@@ -20,7 +20,7 @@ links.forEach(function(link) {
         content.classList.add("animate__animated");
 
         setTimeout(function(){
-            location.href = "/";
+            location.href = "/Boletin";
         },600);
 
         
